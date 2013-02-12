acafc13b46d6dd2d9bf76ad98f6a001e6a4907ed
Chris Dent <chris.dent@gmail.com>
2013-02-12 15:09:22 +0000

    Simplistic testing of writable store.
    Basic concept now sound, by virtue of resource_filename.
    
    Now to add in the safety catches.

--- test/test_write_store.py
+++ test/test_write_store.py
@@ -1,14 +1,59 @@
 
+import os
+import shutil
 from tiddlyweb.config import config
 
 from tiddlyweb.store import Store
 
-def test_create_store():
+from tiddlyweb.model.bag import Bag
+from tiddlyweb.model.tiddler import Tiddler
+
+
+def setup_module(module):
+    try:
+        shutil.rmtree('testpackage/resources/store')
+    except:  # not there
+        pass
     environ = {'tiddlyweb.config': config}
     store = Store('tiddlywebplugins.pkgstore',
             {'package': 'testpackage', 'read_only': False},
             environ)
-    environ['tiddlyweb.store'] = store
+    module.store = store
 
-    print store
 
+def test_base_structure():
+    assert os.path.exists('testpackage/resources/store')
+    assert os.path.isdir('testpackage/resources/store')
+    assert os.path.exists('testpackage/resources/store/recipes')
+    assert os.path.isdir('testpackage/resources/store/recipes')
+    assert os.path.exists('testpackage/resources/store/bags')
+    assert os.path.isdir('testpackage/resources/store/bags')
+    assert os.path.exists('testpackage/resources/store/users')
+    assert os.path.isdir('testpackage/resources/store/users')
+
+
+def test_put_bag():
+    bag = Bag('testone')
+    store.put(bag)
+    assert os.path.exists('testpackage/resources/store/bags/testone')
+    assert os.path.isdir('testpackage/resources/store/bags/testone')
+
+
+def test_put_tiddler():
+    tiddler = Tiddler('tiddlerone', 'testone')
+    tiddler.text = 'oh hi'
+    store.put(tiddler)
+    assert os.path.exists(
+            'testpackage/resources/store/bags/testone/tiddlers/tiddlerone')
+    assert os.path.isdir(
+            'testpackage/resources/store/bags/testone/tiddlers/tiddlerone')
+    with open(
+            'testpackage/resources/store/bags/testone/tiddlers/tiddlerone/1') as tiddler_file:
+        content = tiddler_file.read().split('\n\n')[1].strip()
+        assert content == 'oh hi'
+
+
+def test_get_tiddler():
+    tiddler = Tiddler('tiddlerone', 'testone')
+    tiddler = store.get(tiddler)
+    assert tiddler.text == 'oh hi'
